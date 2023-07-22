6bc923946bb484a3ca73a8f09c581072e0710060
basilinnia <dev.ecemk01@gmail.com>
2023-07-22 11:47:59 +0300

    Initial update

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
 import { useState } from "react";
 import { generate } from "random-words";
 export default function Home() {
-  const initialPrompt = generate(10).toString().trim(",");
+  const [initialPrompt, setInitialPrompt] = useState(generate(10).toString().trim(","));
   const [promptValue, setPromptValue] = useState(initialPrompt);
   const [negativePrompt, setNegativePrompt] = useState("");
   const [stylePrompt, setStylePrompt] = useState("");
+  const [toolPrompt, setToolPrompt] = useState("");
   const [prediction, setPrediction] = useState(null);
   const [error, setError] = useState(null);
 
   const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const { negative_prompt } = Object.fromEntries(formData.entries());
+    const {negative_prompt} = Object.fromEntries(formData.entries());
 
     const response = await fetch("/api/predictions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ prompt: promptValue + stylePrompt, negative_prompt }),
+      body: JSON.stringify({prompt: promptValue +"in the style of "+ stylePrompt + "painted with using/in: " + toolPrompt, negative_prompt}),
     });
 
     let prediction = await response.json();
@@ -26,9 +27,6 @@ export default function Home() {
       setError(prediction.detail);
       return;
     }
-
-    setPrediction(prediction);
-
     const timer = setInterval(async () => {
       const response = await fetch("/api/predictions/" + prediction.id);
       prediction = await response.json();
@@ -38,18 +36,18 @@ export default function Home() {
       }
       setPrediction(prediction);
       if (prediction.status === "succeeded") {
+        setPrediction(prediction);
         clearInterval(timer);
       }
     }, 1000);
   };
-
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-300 to-purple-500 py-6 flex flex-col justify-center items-center">
-              <div className="mb-4">
-            Initial Prompts
-          <p className="bg-gray-200 rounded px-2 py-1 text-center">{initialPrompt}</p>
-        </div>
-      <div className="w-full bg-white shadow-md rounded p-4 mx-20">
+      <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-600 py-6 flex flex-col justify-center items-center">
+      <div className="mb-4 text-white text-xl font-semibold">
+        Initial Prompts
+        <p className="bg-gray-200 rounded px-2 py-1 text-black text-center">{initialPrompt}</p>
+      </div>
+      <div className="w-2/3 bg-white shadow-md rounded-lg p-6 mx-20">
         <form onSubmit={onSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="prompt">
@@ -57,7 +55,7 @@ export default function Home() {
             </label>
             <textarea
               name="prompt"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 leading-tight focus:outline-none focus:shadow-outline"
               value={promptValue}
               onChange={(e) => setPromptValue(e.target.value)}
             ></textarea>
@@ -68,7 +66,7 @@ export default function Home() {
             </label>
             <textarea
               name="negative_prompt"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 leading-tight focus:outline-none focus:shadow-outline"
               value={negativePrompt}
               onChange={(e) => setNegativePrompt(e.target.value)}
             ></textarea>
@@ -79,21 +77,32 @@ export default function Home() {
             </label>
             <textarea
               name="style"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-blue-500 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 leading-tight focus:outline-none focus:shadow-outline"
               value={stylePrompt}
               onChange={(e) => setStylePrompt(e.target.value)}
             ></textarea>
           </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tool">
+              Tool Prompt
+            </label>
+            <textarea
+              name="tool"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+              value={toolPrompt}
+              onChange={(e) => setToolPrompt(e.target.value)}
+            ></textarea>
+          </div>
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            className="bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
           >
             GENERATE
           </button>
         </form>
       </div>
       {prediction && (
-        <div className="bg-white shadow-md rounded p-4 mt-4">
+        <div className="bg-white shadow-md rounded-lg p-4 mt-4">
           {prediction.output && (
             <div className="grid grid-cols-2 gap-4">
               {prediction.output.map((imgSrc, index) => (
@@ -103,7 +112,7 @@ export default function Home() {
                   alt={`output ${index + 1}`}
                   width={340}
                   height={340}
-                  className="rounded"
+                  className="rounded-lg"
                 />
               ))}
             </div>
